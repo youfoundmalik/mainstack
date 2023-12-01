5dde53aebff0c2013b347a51185f27e696002fa2
youfoundmalik <godswillugochukwu88@gmail.com>
2023-12-01 23:09:33 +0100

    prevent responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { useFetchUser } from 'hooks/useFetchUser';
+import { useMediaQuery } from '@chakra-ui/react';
 import RevenuePage from 'pages/dashboard/revenue';
 
 const App = () => {
-  useFetchUser();
-  return <RevenuePage />;
+  const [isDesktop] = useMediaQuery('(min-width: 1050px)');
+
+  return (
+    <>
+      {isDesktop ? (
+        <RevenuePage />
+      ) : (
+        <div className='w-screen h-screen flex items-center justify-center'>
+          <p className='w-fit text-lg font-bold'>
+            Please open on a desktop device
+          </p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default App;
--- src/components/shared/header/index.tsx
+++ src/components/shared/header/index.tsx
@@ -1,9 +1,11 @@
+import { useFetchUser } from 'hooks/useFetchUser';
 import HeaderActions from './Actions';
 import Navigation from './Navigation';
 import './index.scss';
 import logo from 'assets/mainstack.png';
 
 const AppHeader = () => {
+  useFetchUser();
   return (
     <div className='app-header m-[20px] sticky top-0 py-[15px] px-[25px] box-border flex justify-between items-center h-[65px]'>
       <div className='app-logo w-[30px]'>
